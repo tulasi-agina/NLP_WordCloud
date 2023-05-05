b8cc39710ed2d6958db2c5232cb34f31e93b90cb
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-04 21:40:44 -0700

    Update WordCloud.py

--- WordCloud.py
+++ WordCloud.py
@@ -10,5 +10,5 @@ st.text("past a Sonnet here")
 txt = st.text_area('Copy-paste a Sonnet here to create a Word Cloud')
 # st.write('you typed:', txt)
 
-my_valence = TextBlob(text)
+my_valence = TextBlob(txt)
 st.write('sentiment of the sonnet is:', my_valence.sentiment)
