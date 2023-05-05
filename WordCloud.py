7d3e1fd055f86b82056c5348fa956f8485b29004
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-04 21:15:31 -0700

    Update WordCloud.py

--- WordCloud.py
+++ WordCloud.py
@@ -5,5 +5,5 @@ st.title("Word Cloud of Shakespeare Sonnets")
 st.header("Sonnet Cloud")
 
 st.text("past a Sonnet here")
-txt = st.text_input()
+txt = st.text_input('enter your sonnet here please)
 st.write('you typed:', txt)
